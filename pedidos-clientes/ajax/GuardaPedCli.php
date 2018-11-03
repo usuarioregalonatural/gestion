9df2262a2493726c2ba3e4e051c9b15fcaf399e0
Victor <webmaster@regalonatural.com>
2018-11-03 22:15:25 +0100

    Primeros accesos a bbdd correctos

--- pedidos-clientes/ajax/GuardaPedCli.php
+++ pedidos-clientes/ajax/GuardaPedCli.php
@@ -6,14 +6,18 @@
  * Date: 02/11/2018
  * Time: 21:44
  */
-/*try {
+try {
     require_once("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+    require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
+    $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $total_pedido_query   = mysqli_query($con, "SELECT sum(precio_tmp) as total from tmp");
+    $row= mysqli_fetch_array($total_pedido_query);
+    $total_pedido = $row['total'];
 } catch (Exception $ex){
     echo 'Error: ' . $ex->getMessage().PHP_EOL;
 }
 
-require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
-*/
+
 /*try{
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 } catch(Exception $e)
@@ -33,6 +37,7 @@ $fecha_prevista=$_POST['fecha_prevista'];
 echo "CLIENTE: ".$id_cliente . PHP_EOL;
 echo "-FECHA PEDIDO: " . $fecha_pedido . PHP_EOL;
 echo "-FECHA PREVISTA: " . $fecha_prevista . PHP_EOL;
+echo "-TOTAL PEDIDO: " . $total_pedido . PHP_EOL;
 
 // Calcular el ID del Pedido
 // $id_pedido=
